44c0a6da3fe9aa134a422ef8b4161322c1af5a09
Harshwardhana Shekhawat <35223826+bannaji@users.noreply.github.com>
2019-11-07 17:29:09 +0530

    Update pruscha.rs

--- pruscha.rs
+++ pruscha.rs
@@ -51,6 +51,10 @@
 % location of what
 - {@ sjt location}
 
++ [*] techology [*]
+% location of what
+- {@ tt location}
+
 + tt
 % location of what
 - {@ tt location}
@@ -73,6 +77,14 @@
 - what do you want to know about sjt?
 - try asking "sjt phone number" or "sjt loaction"
 
++ [*] allmart [*]
+-all mart is vit's own shopping hub where you can find various types of supplies and things you need. it is just outside the gate 2 of vit.
+
++ [*] cat [*]
+- CAT is the sessional conducted by VIT. It is taken twice and CAT 2 is openbook exam.
+
+
+
 + [*] tt [*]
 - what do you want to know about tt?
 - try asking "tt phone number" or "tt loaction"
@@ -94,6 +106,78 @@
 
 + [*] counselling [*] {weight= 10}
 - what do you want to know about counselling
+
++i made mistakes in my application. will there be correction dates for viteee
+- candidates can make changes in the application form by logging using their application number. they can make changes except in email id, mobile number, address details etc. and submit it till february 28, 2018, which is the last date.
+
++when can we download the admit card of viteee
+- to download the admit card of viteee, candidates will had to first book their slots and then they will be able to download their admit card
+
++ what are the placement chances if i study in vit
+- the university holds a good track record of placements at good companies and packages. to know more, candidates can check the university website.
++which branch is best at vit and which campus
+-as per student perceptions and reports, the most preferred branches are computer science engineering, mechanical engineering, electronics and communication engineering. the preferred campus by most students is vit vellore since it is the oldest campus while the others also are equally good..
+
++which is the best vit campus
+-according to careers360 rating, vit vellore rates higher than the other campuses when it comes to placement, student life, infrastructure, college life, faculty, hostel, affordability, industry exposure etc.
+
+
+
+
++ what is the counselling fee and how to apply for viteee
+-candidates will have to attend the counselling of viteee with a demand draft of rs 50,000 drawn in favour of vit university payable at vellore. this amount is inclusive of rs 12000 (service tax) towards vit counselling and fee.
+
++is there different counselling for the different campuses
+- - no, there is a single window counselling across all campuses of vit. candidates can attend the counselling at any of the campuses.
+
++what is the procedure for viteee counselling
+-the procedure for viteee includes registration, verification of documents, allotment of branch, uploading of photograph, signature, finger print, student profile which includes uploading documents and hostel enquiry
+
++what documents are required for viteee counselling
+-candidates need to produce the original and photocopies of viteee admit card, counselling card, copy of viteee result, class x board certificate, marks sheet of qualifying exam, class xii hall ticket, community certificate (if applicable), nativity certificate, demand draft of rs 50,000, undertaking form.
+
++i cannot be present for viteee counselling. who can attend on my behalf
+-as specified, candidates will have to attend the counselling themselves but in case due to certain circumstances, if they fail to, they will have to intimate the university regarding the same and ask for a subsequent date and time to attend the counselling.
+
++how much initial fee has to be paid for viteee counselling
+-candidates need to pay rs 50,000 for counselling in the form of dd drawn in favour of vit university payable at vellore.
+
++what is the eligibility criteria to apply for viteee
+- for viteee, candidates need to have passed 10+2 from state or cbse board with 60% marks in physics, chemistry and mathematics.
+
++-what is the age limit for viteee
+- candidates should be born on or after july 1, 1996 as per viteee eligibility criteria 2018.’
++how many attempts can i make for viteee
+-candidates can appear in viteee only once.
++i am a diploma student. can i appy for viteee
+-no, diploma candidates cannot apply for viteee 2018 exam.
+
++how much should i have in class 12 to apply for viteee
+- candidates must have 60% in physics, chemistry and mathematics/ biology in aggregate in 10+2. however, candidates belonging to sc/ st categories and north-east need 50% in aggregate in the above mentioned subjects.
+
++ i have taken a drop. will it affect my admission
+- in this case, candidates need to check the eligibility of 10+2 passing year, age limit as they have to be born on or after july 1, 1996 and class xii qualifying exams details. if they meet the criteria, they can apply. a drop will not make a difference in admissions.
+
++if i have a gap after class 10, can i apply for viteee
+-candidates can appear in viteee only if they meet the eligibility criteria of passing year, qualifying marks and age limit.
+
++i have failed in class 12 and appeared for the exam again. can i apply for viteee
+-- in such a case, candidates need to check the age limit as he or she must have completed 17 years as on july 1, 1996 and must have secured minimum 60% marks in physics, chemistry & mathematics in 10+2 or equivalent exam. minimum 50% marks are required in class 10+2 if candidates belong to sc or st categories.
+
++if i am biology student but i have studied physics & chemistry, can i apply for viteee which courses will i be eligible for
+-candidates with physics, chemistry and biology will be eligible for admission to b.tech in bioengineering, biotechnology, ece with spl. in biomedical engineering, b.tech (computer science and engg.) with specialty in bioinformatics but after joining, candidates will have to register for mathematics as bridge course and this is compulsory.
+
++is there any eligibility relaxation in viteee and for whom
+-there is an eligibility relaxation for jammu & kashmir candidates, reserved category candidates like sc and st and north east candidates coming from arunachal pradesh, assam, manipur, meghalaya, mizoram, nagaland, sikkim and tripura. such candidates will have to score 50% aggregate marks in physics, chemistry, mathematics/ biology in 10+2 or equivalent exam.
+
++[*] i belong to a north east state [*]
+-candidates from north east need 50% aggregate in 10+2 or equivalent exam in physics, chemistry, mathematics or biology.
++what are different reservation categories for admission through viteee
+-there is no reservation criteria for anyone in viteee.
++is there an nri quota for viteee
+-no, there is no quota for nri's as admissions are open for all. pio/ oci candidates should have studied in schools located in india in the preceding two years.
+
+
 
 + *
 - Sorry did not get what you said
