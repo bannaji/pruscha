4217fee8287a96b2cb99ae4d8432014f99cb2350
Harshwardhana Shekhawat <35223826+bannaji@users.noreply.github.com>
2019-11-08 14:03:18 +0530

    Create projectt.rs

---
+++ projectt.rs
@@ -0,0 +1,142 @@
+! version = 2.0
+! sub i'm     = i am
+! sub i'd     = i would
+! sub i've    = i have
+! sub i'll    = i will
+! sub don't   = do not
+! sub isn't   = is not
+! sub you'd   = you would
+! sub you're  = you are
+! sub you've  = you have
+! sub you'll  = you will
+! sub what's  = what is
+! sub whats   = what is
+! sub what're = what are
+! sub what've = what have
+! sub what'll = what will
+! sub who's   = who is
+
++ hello
+- hello, you can ask me about vit?
+
++ hi*
+@ hello
+
++ my name is *
+- nice to meet you <star>
+- nice meeting you <star>
+
++ thank you
+- you are welcome
+- welcome
+- no problem
+- no worries
+
++ [*] sjt location [*] {weight=10}
+- it is located near the lakshmibai block.
+
++ where is sjt {weight=10}
+- {@ sjt location}
+
++ [*] scope [*]
+- scope is known as scool of computer science and engineering, its head office is loacted on SJT third floor.
+
++ [*] computer [*]
+- The computer science branch school SCOPE is loacted in sjt and its HOD is prof santhi v.
+
++ [*] location [*]
+-location of what
+
++ sjt
+% location of what
+- {@ sjt location}
+
++ [*] techology [*]
+% location of what
+- {@ tt location}
+
++ [*] techology [*]
+- {@ tt }
+
++ tt
+% location of what
+- {@ tt location}
+
++ [*] tt location[*]{weight=10}
+- tt is located neat the food court.
+
++[*] hod  [*] {weight=2}
+- which school hod you want to know about
+
++ scope
+% which school hod you want to know about
+- @ [*] computer [*]
+
++ site
+% which school hod you want to know about
+- prof jayakumar
+
++ [*] sjt [*]
+- what do you want to know about sjt?
+- try asking "sjt phone number" or "sjt loaction"
+
++ [*] (allmart| shopping | all mart) [*]
+-all mart is vit's own shopping hub where you can find various types of supplies and things you need. it is just outside the gate 2 of vit.
+
++ [*] cat [*]
+- CAT is the sessional conducted by VIT. It is taken twice and CAT 2 is openbook exam.
+
+
++ [*] tt [*]
+- what do you want to know about tt?
+- try asking "tt phone number" or "tt loaction"
+
++ where is counselling going on
+- anna auditorium
+
++ [*] anna [*]
+- anna auditorium is in front on Main building's second enterance, you can reach there by entering Main building and exiting from other side.
+
++ [*] counselling schedule [*]
+- The counselling schedule has been sent to your mail you can check it there.
+ 
++ [*] time [*] sjt [*]
+- the opening time of sjt is from 7 am to 7:30 pm
+
++ [*] (chancellor|g visshvanathan|chancellor*) [*]
+- Dr. G vishvanathan is our chancellor. our chancellor's office is located in Main building on the ground floor, you will have to take an appointment from MB in order to meet him.
+
++ [*] counselling [*] {weight= 10}
+- what do you want to know about counselling
+
++ when can we download the admit card of viteee
+- to download the admit card of viteee, candidates will had to first book their slots and then they will be able to download their admit card
+
++ what are the placement chances if i study in vit
+- the university holds a good track record of placements at good companies and packages. to know more, candidates can check the university website.
+
++ which branch is best at vit and which campus
+- as per student perceptions and reports, the most preferred branches are computer science engineering, mechanical engineering, electronics and communication engineering. the preferred campus by most students is vit vellore since it is the oldest campus while the others also are equally good..
+
++ which is the best vit campus
+- according to careers360 rating, vit vellore rates higher than the other campuses when it comes to placement, student life, infrastructure, college life, faculty, hostel, affordability, industry exposure etc.
+
+
++ [*] i have taken a drop [*]
+- in this case, candidates need to check the eligibility of 10+2 passing year, age limit as they have to be born on or after july 1, 1996 and class xii qualifying exams details. if they meet the criteria, they can apply. a drop will not make a difference in admissions.
+
+
++ [*] i belong to a north east state [*]
+- candidates from north east need 50% aggregate in 10+2 or equivalent exam in physics, chemistry, mathematics or biology.
++ what are different reservation categories for admission through viteee
+- there is no reservation criteria for anyone in viteee.
++ is there an nri quota for viteee
+- no, there is no quota for nri's as admissions are open for all. pio/ oci candidates should have studied in schools located in india in the preceding two years.
+
+
+
++ *
+- Sorry did not get what you said
+- I am afraid that i can not understand you
+- I did not get it
+- Sorry can you please elaborate that for me
